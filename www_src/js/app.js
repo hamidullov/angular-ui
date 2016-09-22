3c7aaca056bc4fb080567aa85797b764fa85ec07
airat <x_airat@mail.ru>
2016-09-22 18:01:16 +0300

    bugfix: templates module

--- www_src/js/app.js
+++ www_src/js/app.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('myApp', ['ui.router', 'myApp.config', 'myApp.translate', 'myApp.locale'])
+    angular.module('myApp', ['ui.router', 'myApp.config', 'myApp.translate', 'myApp.locale', 'templates'])
     .run(function(){
     })
 })();
